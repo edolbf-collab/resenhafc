078168f415a096cee52b53a34af14cddaec38998
edolbf-collab <edolbf@gmail.com>
2026-07-21 14:10:31 -0300

    Update VAPID public key in supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,7 +2,7 @@ window.RESENHA_CONFIG = {
   supabaseUrl: "https://xvwgrpnioaqyijhzqxqh.supabase.co",
   supabasePublishableKey: "sb_publishable_d0sXTfLf9ZNhDNbDfVhRYg_ZfNjQscs",
   googleClientId: "450022998573-t005205phn1crgmbk7g7sddg3qj9b0jp.apps.googleusercontent.com",
-  vapidPublicKey: "BBpKmFcrqGPXT2gI1aFATOqmzqT06djAW4muJDgCXU8uLxTCmjWitfvTEQXHnRvv77PkLhYw8_iySXn0leObpDE",
+  vapidPublicKey: "BFbtcPw10wYze6KTaYqOa0zw3Urz3mTx4j9030LA3F8LUILtq2WN1MpOx0AlGjRWFRjFbHZNNRqFCk1QK49Sj-c",
   authRedirectUrl: new URL("./", window.location.href).href,
   appName: "Resenha FC"
 };
